26e8cc0a541683b6d5c7a2679279e488fec5656d
Angel <angel.ortiz@societao.com>
2019-01-30 11:00:15 -0600

    Initial Commit

---
+++ POCs FVP iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POCs FVP iOS
+//
+//  Created by User on 30/01/19.
+//  Copyright © 2019 POCs FVP iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
